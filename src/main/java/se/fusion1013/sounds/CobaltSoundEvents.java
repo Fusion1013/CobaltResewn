1a2c3e230272ed563986e7fe549c737443c60454
Fusion1013 <william.reutmer@gmail.com>
2025-03-09 05:02:03 +0100

    Update sound events

--- src/main/java/se/fusion1013/sounds/CobaltSoundEvents.java
+++ src/main/java/se/fusion1013/sounds/CobaltSoundEvents.java
@@ -14,12 +14,14 @@ public class CobaltSoundEvents {
     public static final SoundEvent LIGHT_HOLDER_LENS_INSERT = register("block.light_holder.lens_insert");
     public static final SoundEvent LIGHT_HOLDER_LENS_REMOVE = register("block.light_holder.lens_remove");
 
-    public static final SoundEvent ILLUSORY_WALL = register("illusory_wall");
+    public static final SoundEvent ILLUSORY_WALL = register("sfx.illusory_wall");
 
     // Mobs
-    public static final SoundEvent RAT_HURT = register("entity.rat.hurt");
-    public static final SoundEvent RAT_AMBIENT = register("entity.rat.ambient");
-    public static final SoundEvent RAT_DEATH = register("entity.rat.death");
+    public static final SoundEvent RAT_HIT = register("entity.rat.hit");
+    public static final SoundEvent RAT_SAY = register("entity.rat.say");
+    public static final SoundEvent RAT_KILL = register("entity.rat.kill");
+
+    public static final SoundEvent ELECTRIC_SPARK = register("sfx.electric.spark");
 
     public static SoundEvent register(String id) {
         Identifier identifier = new Identifier(Main.MOD_NAMESPACE + ":" + id);
