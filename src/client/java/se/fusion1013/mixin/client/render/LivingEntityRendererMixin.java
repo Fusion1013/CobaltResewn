4f6b67dbaa4b4c6ddb480c0dd2c60f244b5ef0e1
Fusion1013 <william.reutmer@gmail.com>
2025-05-05 09:06:26 +0200

    Remove content

--- src/client/java/se/fusion1013/mixin/client/render/LivingEntityRendererMixin.java
+++ src/client/java/se/fusion1013/mixin/client/render/LivingEntityRendererMixin.java
@@ -16,22 +16,4 @@ import se.fusion1013.Main;
 @Mixin(LivingEntityRenderer.class)
 public abstract class LivingEntityRendererMixin<T extends LivingEntity> extends EntityRendererMixin<T> {
 
-    @Inject(method = "getRenderLayer", at = @At("HEAD"), cancellable = true)
-    protected void getRenderLayer(T entity, boolean showBody, boolean translucent, boolean showOutline, CallbackInfoReturnable<RenderLayer> cir) {
-        Identifier identifier = getTexture(entity);
-        if (entity instanceof PlayerEntity) {
-            Main.LOGGER.info("ShowBody: " + showBody + ", Translucent: " + translucent + ", ShowOutline: " + showOutline);
-            // cir.setReturnValue(RenderLayer.getItemEntityTranslucentCull(identifier));
-        }
-    }
-// Ordinal 9
-    @ModifyVariable(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "STORE"), ordinal = 0)
-    public boolean render(boolean original) {
-        return original;
-    }
-
-    @Inject(method = "isVisible", at = @At("RETURN"), cancellable = true)
-    protected void isVisible(T entity, CallbackInfoReturnable<Boolean> cir) {
-        // cir.setReturnValue(false);
-    }
 }
